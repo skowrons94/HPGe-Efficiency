36e170e61c5856610149737ab140268cf02fed54
Jakub Skowronski <jakub.skowronski@protonmail.com>
2023-06-01 11:05:55 +0200

    Fix for 22Na

--- ResidueNa.py
+++ ResidueNa.py
@@ -14,27 +14,27 @@ class ResidueNa( ):
         residues = [ ]
         eff = Efficiency( params )
         key1, key2 = "511", "1274.57"
-        eff_avg = self.data.data[key1].Yield[0]/( ( 1 - eff.effTot( 1.27457 ) )*1.807 )
+        eff_avg = self.data.values[key1].Yield[0]/( ( 1 - eff.effTot( 1.27457 ) )*1.807 )
         eff_tot_avg = eff_avg/( np.exp( params["k1"] + params["k2"]*np.log( 0.511 ) + params["k3"]*pow( np.log( 0.511 ), 2 ) ) )
         Yield = eff.effPeak( float( key2 )/1000 )*( 1 - 1.807*eff_tot_avg )
-        residues.append( pow( ( Yield - self.data.data[key2].Yield[0] )/self.data.data[key2].YieldErr[0], 2 ) )
+        residues.append( pow( ( Yield - self.data.values[key2].Yield[0] )/self.data.values[key2].YieldErr[0], 2 ) )
         return residues
     
     def get_disc( self, params ):
         disc = [ ]
         eff = Efficiency( params )
         key1, key2 = "511", "1274.57"
-        eff_avg = self.data.data[key1].Yield[0]/( ( 1 - eff.effTot( 1.27457 ) )*1.807 )
+        eff_avg = self.data.values[key1].Yield[0]/( ( 1 - eff.effTot( 1.27457 ) )*1.807 )
         eff_tot_avg = eff_avg/( np.exp( params["k1"] + params["k2"]*np.log( 0.511 ) + params["k3"]*pow( np.log( 0.511 ), 2 ) ) )
         Yield = eff.effPeak( float( key2 )/1000 )*( 1 - 1.807*eff_tot_avg )
-        disc.append( ufloat( ( Yield - self.data.data[key2].Yield[0] )/self.data.data[key2].Yield[0], 0 ) )
+        disc.append( ufloat( ( Yield - self.data.values[key2].Yield[0] )/self.data.values[key2].Yield[0], 0 ) )
         return disc
     
     def get_yields( self, params ):
         yields = [ ]
         eff = Efficiency( params )
         key1, key2 = "511", "1274.57"
-        eff_avg = self.data.data[key1].Yield[0]/( ( 1 - eff.effTot( 1.27457 ) )*1.807 )
+        eff_avg = self.data.values[key1].Yield[0]/( ( 1 - eff.effTot( 1.27457 ) )*1.807 )
         eff_tot_avg = eff_avg/( np.exp( params["k1"] + params["k2"]*np.log( 0.511 ) + params["k3"]*pow( np.log( 0.511 ), 2 ) ) )
         yields.append( ufloat( eff.effPeak( float( key2 )/1000 )*( 1 - 1.807*eff_tot_avg ), 0 ) )
         return yields
@@ -48,7 +48,7 @@ class ResidueNa( ):
     
     def get_data( self ):
         key = "1274.57"
-        data = [ ufloat( self.data.data[key].Yield[0], self.data.data[key].YieldErr[0] ) ]
+        data = [ ufloat( self.data.values[key].Yield[0], self.data.values[key].YieldErr[0] ) ]
         return data
     
     def get_energies( self ):
